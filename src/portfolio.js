71a40ee9be18d2320d0ff8b927f0772e2d779803
sami-ennedoui <samiennedoui@proton.me>
2025-10-27 22:24:02 +0100

    changes in the projects and the engineering course tabs

--- src/portfolio.js
+++ src/portfolio.js
@@ -117,30 +117,31 @@ const competitiveSites = {
 const degrees = {
   degrees: [
     {
-      title: "Indian Institute of Information Technology Kurnool",
-      subtitle: "B.Tech. in Computer Engineering",
-      logo_path: "iiitk_logo.png",
-      alt_name: "IIITDM Kurnool",
-      duration: "2016 - 2020",
+      title: "Toulouse INP - ENSEEIHT",
+      subtitle:
+        " Electronics Electrical Energy and Automation (3EA) departement ",
+      logo_path: "n7logo.png",
+      alt_name: "INPT ENSEEIHT",
+      duration: "2025 - now",
       descriptions: [
-        "⚡ I have studied basic software engineering subjects like DS, Algorithms, DBMS, OS, CA, AI etc.",
-        "⚡ Apart from this, I have done courses on Deep Learning, Data Science, Cloud Computing and Full Stack Development.",
-        "⚡ I was selected for Merit cum Means Scholarship which is given to top 10% of students in college. I have received award from respected director for consistently best performance in academics.",
+        "⚡ I am currently studying the basics of electrical engineering.",
+        "⚡ I am also working on various projects on my own to gain more practical knowledge in the fields that interest me the most.",
       ],
-      website_link: "http://iiitk.ac.in",
+      website_link: "https://www.enseeiht.fr/",
     },
     {
-      title: "Indiana University Bloomington",
-      subtitle: "M.S. in Computer Science",
-      logo_path: "iu_logo.png",
-      alt_name: "Indiana University Bloomington",
-      duration: "2021 - 2023",
+      title: "CPGE Centre - Tétouan",
+      subtitle: "Math and Physics major",
+      logo_path: "tet1.png",
+      alt_name: "CPGE Centre - Tétouan",
+      duration: "2023 - 2025",
       descriptions: [
-        "⚡ I have taken varity of courses related to Artificial Intelligence which correspond to Explainable AI, Graph Machine Learning, Computer Vision etc.",
-        "⚡ Apart from this, I have also done research assistantship. As part of it, I have worked on creating new algorithms in Graph ML and Network Science.",
-        "⚡ During my time at university, I was also associated with multimedia department. As part of it, I have worked on some documentry films and interviews.",
+        "⚡ Acquired deep knowledge and fundementals in both maths and physics.",
+        "⚡ Learned to work efficiently and flourish in stressful environemnts. ",
+        "⚡ Acquired basic knowledge in the Python programming language which I later applied in my final Project to combine my knowledge in math and programming.",
       ],
-      website_link: "https://www.indiana.edu/",
+      website_link:
+        "https://www.linkedin.com/school/cpge-tetouan/?originalSubdomain=fr",
     },
   ],
 };
